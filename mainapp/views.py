99efcc3ff7ac9ae1e56006839d9838ed7a39f965
Shrijak Dahal <dahalshrijak@gmail.com>
2020-10-05 21:10:41 +0545

    Update views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,22 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session,sessionmaker
-from django.http import HttpResponse
+from sqlalchemy.orm import scoped_session, sessionmaker
+import requests
 
+# Create your views here.
 engine = create_engine("postgres://vrxmsncxrvcjor:1594c4b320f5dc33ef59ed2b04d5746ff36ed630591f77c38a8a9dcd21d5b400@ec2-34-232-212-164.compute-1.amazonaws.com:5432/dstmff6bkopfn")
-db=scoped_session(sessionmaker(bind=engine))
+db = scoped_session(sessionmaker(bind=engine))
 
 
 def main(request):
-    return render(request,"mainapp/layout.html")
+    res = requests.get("https://nepalcorona.info/api/v1/data/nepal")
+    data = res.json()
+    return render(request, "speechtotext/index1.html",{"positive": data["tested_positive"], "recovered": data["recovered"], "deaths": data["deaths"]})
 
-def download(request,text):
 
-    if request.method=="POST":
-        blob=request.body
-        datas=db.execute("SELECT * FROM sound")
+def download(request, text):
+    if request.method == 'POST':
+        blob = request.body
         db.execute("INSERT INTO sound (speech, text) VALUES (:speech, :text)", {
-                  'speech': blob, 'text': text})
+                   'speech': blob, 'text': text})
         db.commit()
-        print(text)
-        return HttpResponse("good")
+    return HttpResponse("<h1>Only apply through post method</h1>")
